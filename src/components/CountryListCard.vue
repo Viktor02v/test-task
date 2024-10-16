6a87c45c4cb16b13e33e8fa2271fa37485d4d327
Viktor Petruk <damnnn261@gmail.com>
2024-10-16 22:35:21 +0300

    make a component for CountryListCard, make some fixation in HomeVue

---
+++ src/components/CountryListCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+  country: Country;
+  index: number;
+}>();
+</script>
+
+
+
+<template>
+
+	<div class="w-[35vw] h-30 border py-5 border-black">
+		<p class="text-center">{{ props.country.name }} - {{ props.index + 1 }}</p>
+	</div>
+</template>
--- src/views/HomeVue.vue
+++ src/views/HomeVue.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted, computed } from 'vue';
+import CountryListCard from '../components/CountryListCard.vue';
 import axios from 'axios';
 
 interface Country {
@@ -21,9 +22,17 @@ const filteredCountries = computed(() => {
 	})
 })
 
+const isLoading = ref(true);
+
 onMounted(async () => {
-	const { data } = await axios.get<Country[]>('https://date.nager.at/api/v3/AvailableCountries');
-	countries.value = data
+	try {
+		const { data } = await axios.get<Country[]>('https://date.nager.at/api/v3/AvailableCountries');
+		countries.value = data;
+	} catch (error) {
+		console.error("Error fetching countries:", error);
+	} finally {
+		isLoading.value = false;
+	}
 });
 
 
@@ -33,29 +42,26 @@ onMounted(async () => {
 	<main id="main">
 		<div class="w-full h-full py-20 container">
 			<div class="flex items-start">
-				<div class="w-full h-[100vh]   flex flex-col">
+				<div class="w-full h-[100vh] flex flex-col">
+					<!-- Search Bar -->
 					<input @input="search = ($event.target as HTMLInputElement).value" type="text" placeholder="Search Input"
 						class="w-[15vw] p-2 border border-black ">
-					<!-- {{ search }} -->
 					<h1 class="mt-5 text-2xl mb-5">Countries List</h1>
 					<!-- Card List -->
 					<div class="overflow-auto h-[60vh]">
-						<div v-for="(country, index) in filteredCountries" :key="index" class="flex w-full  flex-col py-3 ">
-							<!-- Card -->
-							<div class="w-[35vw] h-30 border py-5 border-black">
-								<p class="text-center">{{ country.name }} - {{ index + 1 }}</p>
+						<div v-if="isLoading">Loading...</div> <!-- Loading State -->
+						<div v-else>
+							<div v-for="(country, index) in filteredCountries" :key="index" class="flex w-full flex-col py-3 ">
+								<!-- Card -->
+								<CountryListCard :country="country" :index="index" />
 							</div>
 						</div>
 					</div>
 				</div>
-
 				<div class="w-full h-full">
 					<h1 class="text-2xl">Random Countries Widget</h1>
-					<div class="w-full h-full border border-[2px] border-black">
-
-					</div>
+					<div class="w-full h-full border border-[2px] border-black"></div>
 				</div>
-
 			</div>
 		</div>
 	</main>
