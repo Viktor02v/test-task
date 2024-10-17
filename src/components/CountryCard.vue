f97fc9725944dcd95f013111dce99ef51875c20a
Viktor Petruk <damnnn261@gmail.com>
2024-10-17 04:43:27 +0300

    use run format

--- src/components/CountryCard.vue
+++ src/components/CountryCard.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
-
 import { RouterLink } from 'vue-router'
 
 const props = defineProps<{
-	country: {
-		name: string
-		countryCode: string
-	}
-	index: number;
-}>();
+  country: {
+    name: string
+    countryCode: string
+  }
+  index: number
+}>()
 </script>
 
-
-
 <template>
-	<router-link :to="`/country/${encodeURIComponent(country.name)}-${country.countryCode}`">
-		<div class="w-[80vw] md:w-[35vw]  h-30 border py-5 border-black">
-			<p class="text-center">{{ props.country.name }} - {{ props.index + 1 }}</p>
-		</div>
-	</router-link>
+  <router-link
+    :to="`/country/${encodeURIComponent(country.name)}-${country.countryCode}`"
+  >
+    <div class="w-[80vw] md:w-[35vw] h-30 border py-5 border-black">
+      <p class="text-center">
+        {{ props.country.name }} - {{ props.index + 1 }}
+      </p>
+    </div>
+  </router-link>
 </template>
--- src/components/HolidayCard.vue
+++ src/components/HolidayCard.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-
 const props = defineProps<{
-	holiday: {
-		localName: string
-		date: string
-	}
-}>();
-
+  holiday: {
+    localName: string
+    date: string
+  }
+}>()
 </script>
 
 <template>
-	<div class="w-[80vw] md:w-[35vw] text-xl h-30 border p-5 border-black">
-		<li>{{ props.holiday.localName }} </li>
-		<li> {{ props.holiday.date }}</li>
-	</div>
+  <div class="w-[80vw] md:w-[35vw] text-xl h-30 border p-5 border-black">
+    <li>{{ props.holiday.localName }}</li>
+    <li>{{ props.holiday.date }}</li>
+  </div>
 </template>
